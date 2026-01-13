666abc010e7aefa0c320383288431a4fdca4a96f
Aviṣa Gupta <avishagupta1290@gmail.com>
2026-01-13 18:36:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,17 +10,8 @@ class Query(BaseModel):
 
 @app.post("/ask")
 def ask(q: Query):
-    return {
-        "answer": answer_question(q.question, q.session_id)
-    }
+    return {"answer": answer_question(q.question, q.session_id)}
 
 @app.get("/")
 def root():
-    return {"status": "Bhagavad Gita chatbot API running"}
-
-if __name__ == "__main__":
-    import os
-    import uvicorn
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("app:app", host="0.0.0.0", port=port, log_level="info")
-
+    return {"status": "AtmaBodh BG chatbot API running"}
