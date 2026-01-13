6de9ecc2d7500951bbf69727b6bd23b921cbf54b
Aviṣa Gupta <avishagupta1290@gmail.com>
2026-01-13 16:21:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from rag import answer_question
+
+app = FastAPI()
+
+class Query(BaseModel):
+    question: str
+    session_id: str = "gita_chat"
+
+@app.post("/ask")
+def ask(q: Query):
+    return {
+        "answer": answer_question(q.question, q.session_id)
+    }
+
+@app.get("/")
+def root():
+    return {"status": "Bhagavad Gita chatbot API running"}
---
+++ rag.py
@@ -0,0 +1,91 @@
+import kagglehub
+from kagglehub import KaggleDatasetAdapter
+
+file_path = "bhagavad_gita_verses.csv"
+
+bg_data = kagglehub.dataset_load(
+  KaggleDatasetAdapter.PANDAS,
+  "madhurpant/bhagavad-gita-verses-dataset",
+  file_path,
+)
+
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_community.docstore.in_memory import InMemoryDocstore
+from langchain_community.document_loaders import DataFrameLoader
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_classic.memory import ChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableLambda
+from langchain_community.vectorstores import FAISS
+from langchain_classic.chains import RetrievalQA
+from google.colab import userdata
+import faiss
+import os
+
+loader = DataFrameLoader(bg_data, page_content_column="translation")
+docs = loader.load()
+
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+
+vectordb = FAISS.from_documents(docs, embeddings)
+
+retriever = vectordb.as_retriever(search_kwargs={"k": 3})
+
+llm = ChatOpenAI(model="gpt-4o-mini",
+                 temperature=0.4)
+
+prompt = ChatPromptTemplate.from_template("""
+You are Vaani, an expert on the Bhagavad Gita.
+Use the conversation history and the context below.
+Answer ONLY using the context.
+Always mention Chapter and Verse (Shlok) and the lines of shloks, in the place where the context from a shlok is explained.
+Be polite, creative in your words, don't repeat same opening or closing lines again and again.
+Also sound interested in the conversation and intellectual.
+
+Conversation History:
+{history}
+
+Context:
+{context}
+
+Question:
+{input}
+
+Answer (with reference):
+""")
+
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+rag_chain = (
+    {
+        "context": (RunnableLambda(lambda x: x["input"]) | retriever | RunnableLambda(format_docs)),
+        "input": RunnableLambda(lambda x: x["input"]),
+        "history": RunnableLambda(lambda x: x.get("history", "")),
+    }
+    | prompt
+    | llm
+)
+
+store = {}
+
+def get_session_history(session_id):
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+chatbot = RunnableWithMessageHistory(
+    rag_chain,
+    get_session_history,
+    input_messages_key="input",
+    history_messages_key="history",
+)
+
+def answer_question(query: str, session_id: str = "bgita_chat"):
+    response = chatbot.invoke(
+        {"input": query},
+        config={"configurable": {"session_id": session_id}}
+    )
+    return response.content
